48cbc80cdd05d51d9c5c61690930414c07de1972
Amin Mirzaei <aminmirzaeidev@gmail.com>
2026-06-10 18:24:22 +0330

    - Add CaptionButton To Controls Folder

---
+++ Tetris/Controls/CaptionButton.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris.Controls
+{
+    public class CaptionButton : System.Windows.Controls.Button
+    {
+
+    }
+}
