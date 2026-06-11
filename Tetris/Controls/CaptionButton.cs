f8afc4aeb25f040a1829ba13b9c6be6f67f2c995
Amin Mirzaei <aminmirzaeidev@gmail.com>
2026-06-11 04:29:39 +0330

    - Add Modes Enum To CpationButton

--- Tetris/Controls/CaptionButton.cs
+++ Tetris/Controls/CaptionButton.cs
@@ -15,40 +15,31 @@ using System.Windows.Shapes;
 
 namespace Tetris.Controls
 {
-    /// <summary>
-    /// Follow steps 1a or 1b and then 2 to use this custom control in a XAML file.
-    ///
-    /// Step 1a) Using this custom control in a XAML file that exists in the current project.
-    /// Add this XmlNamespace attribute to the root element of the markup file where it is 
-    /// to be used:
-    ///
-    ///     xmlns:MyNamespace="clr-namespace:Tetris.Controls"
-    ///
-    ///
-    /// Step 1b) Using this custom control in a XAML file that exists in a different project.
-    /// Add this XmlNamespace attribute to the root element of the markup file where it is 
-    /// to be used:
-    ///
-    ///     xmlns:MyNamespace="clr-namespace:Tetris.Controls;assembly=Tetris.Controls"
-    ///
-    /// You will also need to add a project reference from the project where the XAML file lives
-    /// to this project and Rebuild to avoid compilation errors:
-    ///
-    ///     Right click on the target project in the Solution Explorer and
-    ///     "Add Reference"->"Projects"->[Browse to and select this project]
-    ///
-    ///
-    /// Step 2)
-    /// Go ahead and use your control in the XAML file.
-    ///
-    ///     <MyNamespace:CaptionButton/>
-    ///
-    /// </summary>
     public class CaptionButton : System.Windows.Controls.Button
     {
+        public enum Modes { None,Close,Maximize,Restore,Minimize}
         static CaptionButton()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(CaptionButton), new FrameworkPropertyMetadata(typeof(CaptionButton)));
+        }
+
+        public static readonly DependencyProperty IconProperty =
+        DependencyProperty.Register(nameof(Icon), typeof(string),
+            typeof(CaptionButton), new PropertyMetadata(""));
+        public string Icon
+        {
+            get => (string)GetValue(IconProperty);
+            set => SetValue(IconProperty, value);
+        }
+
+        public static readonly DependencyProperty IconSizeProperty =
+            DependencyProperty.Register(nameof(IconSize), typeof(double),
+                typeof(CaptionButton), new PropertyMetadata(16.0));
+
+        public double IconSize
+        {
+            get => (double)GetValue(IconSizeProperty);
+            set => SetValue(IconSizeProperty, value);
         }
     }
 }
