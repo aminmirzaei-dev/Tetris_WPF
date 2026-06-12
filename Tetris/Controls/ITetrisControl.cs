836f89ead88484e8a1c26b400257363260c54699
Amin Mirzaei <aminmirzaeidev@gmail.com>
2026-06-12 03:39:08 +0330

    - Add ITetrisControl To Controls Folder

---
+++ Tetris/Controls/ITetrisControl.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris.Controls
+{
+    public interface ITetrisControl
+    {
+        Tetris.Controls.Styles Style { get; set; }
+        System.Windows.Media.Color Palette { get; set; }
+    }
+}
