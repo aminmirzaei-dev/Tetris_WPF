2c29867d8f6f8cb955a1e7d255b827ba5a71fa2f
Amin Mirzaei <aminmirzaeidev@gmail.com>
2026-06-27 05:37:38 +0330

    - Update FailureGame MessageBox (Add Load Sound)

--- Tetris/Messages/FailureGame.xaml.cs
+++ Tetris/Messages/FailureGame.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Media;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -22,6 +23,27 @@ namespace Tetris.Messages
         public FailureGame()
         {
             InitializeComponent();
+            Loaded += FailureGame_Loaded;
+        }
+
+        private void FailureGame_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.PlaySound();
+        }
+
+        private void PlaySound()
+        {
+            System.Windows.Resources.StreamResourceInfo splashSoundResource = Application.GetResourceStream(new Uri("Sounds/FailureGame.wav", UriKind.Relative));
+            if (splashSoundResource != null)
+            {
+                SoundPlayer player = new SoundPlayer(splashSoundResource.Stream);
+                player.Play();
+            }
+        }
+
+        private void OkButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
