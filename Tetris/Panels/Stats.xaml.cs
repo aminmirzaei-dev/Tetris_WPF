dff7367843fa5d836980e800632f2c51ed6c35b8
Amin Mirzaei <aminmirzaeidev@gmail.com>
2026-06-28 04:49:29 +0330

    - Add Base Properties To Stats Panel

--- Tetris/Panels/Stats.xaml.cs
+++ Tetris/Panels/Stats.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Tetris.Controls;
 
 namespace Tetris.Panels
 {
@@ -24,5 +25,47 @@ namespace Tetris.Panels
         {
             InitializeComponent();
         }
+
+        public static readonly DependencyProperty DarkModeProperty =
+   DependencyProperty.Register(nameof(DarkMode), typeof(bool),
+       typeof(Stats), new PropertyMetadata(false));
+
+        public bool DarkMode
+        {
+            get => (bool)GetValue(DarkModeProperty);
+            set => SetValue(DarkModeProperty, value);
+        }
+
+        public static readonly DependencyProperty ScoreValueProperty =
+   DependencyProperty.Register(nameof(ScoreValue), typeof(string),
+       typeof(Stats), new PropertyMetadata("12,300"));
+
+        public string ScoreValue
+        {
+            get => (string)GetValue(ScoreValueProperty);
+            set => SetValue(ScoreValueProperty, value);
+        }
+
+        public static readonly DependencyProperty LevelValueProperty =
+  DependencyProperty.Register(nameof(LevelValue), typeof(string),
+      typeof(Stats), new PropertyMetadata("7"));
+
+        public string LevelValue
+        {
+            get => (string)GetValue(LevelValueProperty);
+            set => SetValue(LevelValueProperty, value);
+        }
+
+        public static readonly DependencyProperty LineValueProperty =
+  DependencyProperty.Register(nameof(LineValue), typeof(string),
+      typeof(Stats), new PropertyMetadata("12,300"));
+
+        public string LineValue
+        {
+            get => (string)GetValue(LineValueProperty);
+            set => SetValue(LineValueProperty, value);
+        }
+
+
     }
 }
