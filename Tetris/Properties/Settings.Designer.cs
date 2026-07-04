778354d9e2b02898940147bc835902833aebfcb7
Amin Mirzaei <aminmirzaeidev@gmail.com>
2026-07-05 03:28:23 +0330

    - Add Options Panel

--- Tetris/Properties/Settings.Designer.cs
+++ Tetris/Properties/Settings.Designer.cs
@@ -25,7 +25,7 @@ namespace Tetris.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
         public bool DarkMode {
             get {
                 return ((bool)(this["DarkMode"]));
@@ -37,7 +37,7 @@ namespace Tetris.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("Persian")]
+        [global::System.Configuration.DefaultSettingValueAttribute("English")]
         public string Language {
             get {
                 return ((string)(this["Language"]));
